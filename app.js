bd11b2650d5978755385494c1512676f28545f1c
pemaga12 <47608569+pemaga12@users.noreply.github.com>
2019-02-15 23:09:21 +0100

    derp

--- app.js
+++ app.js
@@ -316,6 +316,24 @@ client.on("ready", () => {
             }
         }
 
+
+        else if (message.content.startsWith(prefix + "derp")) { 
+            let Canalvoz = message.member.voiceChannel;
+            if (!Canalvoz || Canalvoz.type !== 'voice') {
+            message.channel.send('¡Necesitas unirte a un canal de voz primero!.').catch(error => message.channel.send(error));
+            } else if (message.guild.voiceConnection) {
+                const dispatcher = message.guild.voiceConnection.playFile(`C:/Users/pedro/Documents/Programacion/Skere/fuente/derp.mp3`);
+                message.channel.send(':no_good: | derp');
+            } else {
+             message.channel.send('Conectando...').then(m => {
+                  Canalvoz.join().then(() => {
+                       m.edit(':no_good: | derp').catch(error => message.channel.send(error));
+                        const dispatcher = message.guild.voiceConnection.playFile(`C:/Users/pedro/Documents/Programacion/Skere/fuente/derp.mp3`);
+                 }).catch(error => message.channel.send(error));
+             }).catch(error => message.channel.send(error));
+            }
+        }
+
         else if (message.content.startsWith(prefix + "vete")) { 
             let Canalvoz = message.member.voiceChannel;
             if (!Canalvoz) {
